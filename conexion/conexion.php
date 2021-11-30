20bb2cdeb0f53309f63780d5acc886db80b1b5c8
Alberto-AFAC <jmondragonescamilla@gmail.com>
2021-11-30 11:46:07 -0600

    conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,16 +1,14 @@
 <?php 
 	
 	//instancia clase mysqli para la conexion al servidor base de datos- 4 parametros 
-	//$conexion = new mysqli('localhost','u683645102_root','Agencia.SCT2021.','u683645102_gestor');
-	      $conexion = new mysqli('localhost','root','','gestor');
-	      $conexion2 = new mysqli('localhost','root','','afac');
+	$conexion = new mysqli('localhost','u683645102_root','Agencia.SCT2021.','u683645102_gestor');
+	    //   $conexion = new mysqli('localhost','root','','gestor');
+	    //   $conexion2 = new mysqli('localhost','root','','afac');
 	//si mustra un errro al momento de querer conectarse jaja
 	if ($conexion->connect_error):
 			echo "Error de Conexión".$conexion->connect_error;
 	endif;
 
-		if ($conexion2->connect_error):
-			echo "Error de Conexión".$conexion2->connect_error;
-	endif;
+	
 
 ?>
