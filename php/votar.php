ab1a292c6c138a4d157735d7e5ec593406d573fd
angelcansecoc <angelcanseco.c@gmail.com>
2021-12-07 18:20:02 -0600

    1

--- php/votar.php
+++ php/votar.php
@@ -15,6 +15,17 @@ if($opcion === 'votar'){
 						echo "1";
 				 		}
 
+	}else if($opcion === 'votarpor'){
+
+     $perid = $_POST['perid'];
+     $idevl = $_POST['idevl'];
+     $idarper = $_POST['idarper'];
+
+    	if(votacionpor($idarper,$perid,$idevl,$conexion)){
+					echo "0";
+				}else{
+						echo "1";
+				 		}
 	}
 
 function votacion($idarper,$perid,$idevl,$conexion){
@@ -40,6 +51,29 @@ function votacion($idarper,$perid,$idevl,$conexion){
 		}else{
 
 		}
+}
 
+function votacionpor($idarper,$perid,$idevl,$conexion){
+	$query="SELECT * FROM votacion 
+			WHERE idasnt = 2 
+			AND idareas = '$idarper' 
+			AND perid = '$perid'
+			AND idevl = '$idevl' 
+			AND estado = 0";
+			$resultado= mysqli_query($conexion,$query);
+		if($resultado->num_rows==0){
 
+		$sql="INSERT INTO votacion VALUES(0,2,$idarper,$perid,$idevl,0)";
+
+		if(mysqli_query($conexion,$sql)){
+		return true;
+		}else{
+		return false;
+		}
+
+		$this->conexion->cerrar();
+
+		}else{
+
+		}
 }
